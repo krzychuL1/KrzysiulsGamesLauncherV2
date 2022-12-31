544b873fc51df0064662212695c4890ad99d5839
KrzychuL_ <krzychugag@gmail.com>
2022-12-31 12:43:58 +0100

    main

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { app, autoUpdater, BrowserWindow, dialog } = require('electron');
 const path = require('path');
-const config = require("../src/config/config.json");
+//const config = require("../src/config/config.json");
 
 Object.defineProperty(app, 'isPackaged', {
   get() {
@@ -8,10 +8,29 @@ Object.defineProperty(app, 'isPackaged', {
   }
 });
 
-const server = config.serverlink;
-const url = `${server}/update/${process.platform}/${app.getVersion()}`;
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
 
-autoUpdater.setFeedURL({ url });
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools();
+};
+
+
 
 setInterval(() => {
   autoUpdater.checkForUpdates()
@@ -60,28 +79,6 @@ autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
   })
 })
 
-
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  app.quit();
-}
-
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
-
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  //mainWindow.webContents.openDevTools();
-};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
