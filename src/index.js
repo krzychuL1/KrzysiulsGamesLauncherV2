d77171fcc44dac762a9f39f6a591dcc598ba8f2e
KrzychuL_ <krzychugag@gmail.com>
2023-01-02 14:28:23 +0100

    main

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ const { app, BrowserWindow, dialog } = require('electron');
 const { cp } = require('fs');
 const path = require('path');
 const config = require("../src/config/config.json");
-const {autoUpdater} = require('electron-updater');
+const { autoUpdater } = require('electron-updater');
+const packagejson = require('../package.json');
+const { mainModule } = require('process');
+
+autoUpdater.autoDownload = true;
+autoUpdater.autoInstallOnAppQuit = false;
+
 
 Object.defineProperty(app, 'isPackaged', {
   get() {
@@ -29,14 +35,53 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  // Open the DevTools.
+  autoUpdater.checkForUpdates();
+  
+  autoUpdater.on('update-available', (info) => {
+  })
+  autoUpdater.on('update-not-available', (info) => {
+    sendStatusToWindow('Update not available.');
+  })
+  autoUpdater.on('error', (message) => {
+    console.error('There was a problem updating the application')
+    console.error(message)
+  })
+  autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Pobrano ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    //sendStatusToWindow(log_message);
+    const pobieraie = {
+      type: 'info',
+      title: 'Pobieranie aktualizacji...',
+      message: log_message,
+    }
+    dialog.showMessageBox(pobieraie)
+  })
+  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+      type: 'question',
+      buttons: ['Restart', 'Później'],
+      title: 'Aktualizacja aplikacji.',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail:
+        'Nowa wersja została pobrana. Czy chcesz zrestartować aplikację aby ją zainstalować?',
+    }
+  
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+      if (returnValue.response === 0) autoUpdater.quitAndInstall()
+    })
+  })
+
+
+  //Open the DevTools.
   //mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', createWindow,)
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -53,67 +98,11 @@ app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
-
 });
 
-autoUpdater.setFeedURL({
-  url: `https://github.com/krzychuL1/KrzysiulsGamesLauncherV2`,
-  provider: 'github',
-  headers: {
-    'Authorization': config.gittoken,
-  },
-  params: {
-    'release': 'latest',
-  },
-});
-
-setInterval(() => {
-autoUpdater.checkForUpdates()
-}, 2000)
 
 
 
-autoUpdater.on('update-available', (info) => {
-  const options = {
-    type: 'info',
-    title: 'Aktualizacja dostępna',
-    message: 'Nowa wersja aplikacji jest dostępna. Chcesz ją zainstalować teraz?',
-    buttons: ['Tak', 'Nie']
-  };
-  dialog.showMessageBox(options, (response) => {
-    if (response === 0) {
-      // użytkownik wybrał opcję "Tak" - pobierz i zainstaluj aktualizację
-      autoUpdater.downloadUpdate();
-    }
-  });
-})
-autoUpdater.on('update-not-available', (info) => {
-  sendStatusToWindow('Update not available.');
-})
-autoUpdater.on('error', (message) => {
-  console.error('There was a problem updating the application')
-  console.error(message)
-})
-autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  sendStatusToWindow(log_message);
-})
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-  const dialogOpts = {
-    type: 'info',
-    buttons: ['Restart', 'Later'],
-    title: 'Application Update',
-    message: process.platform === 'win32' ? releaseNotes : releaseName,
-    detail:
-      'A new version has been downloaded. Restart the application to apply the updates.',
-  }
-
-  dialog.showMessageBox(dialogOpts).then((returnValue) => {
-    if (returnValue.response === 0) autoUpdater.quitAndInstall()
-  })
-})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
